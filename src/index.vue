08628cf29db38ad74814d98bbe9c878043d8f3f7
york.chen <378157669@qq.com>
2020-07-23 09:48:48 +0800

    Create index.vue

---
+++ src/index.vue
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from '@/app.vue'
+import router from '@/routes'
+import store from './store'
+import '@/app.scss'
+import '@/config'
+import tool from './libs/tool'
+import Vant from '@/vendor/vant'
+import viewportUnitsBuggyfill from 'viewport-units-buggyfill'
+import filters from '@/libs/filters'
+Vue.use(Vant);
+Vue.prototype.$tool = tool;
+filters(Vue);
+window.onload = function () {
+    viewportUnitsBuggyfill.init({
+        hacks: viewportUnitsBuggyfill.hacks
+    });
+}
+new Vue({
+    router,
+    store,
+    el: '#app',
+    components: {App},
+    template: '<App/>'
+});
